1d319f463487bf2359269cf06812466e14efc9a6
Stuart McKay <stuart51994@gmail.com>
2020-04-02 01:07:43 -0700

    successfully pull random quote from stormconsultancy.co.uk API and display return data in console

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
-const Quote = () => (
-  <p>Quote</p>
-);
+const Quote = () => {
+
+  const [hasError, setHasError] = useState(false);
+  const [quoteText, setQuoteText] = useState('');
+  const [quoteAuthor, setQuoteAuthor] = useState('');
+
+  const onClick = () => (
+    console.log("Button has been clicked")
+  )
+
+  const fetchQuote = async () => {
+    const res = await fetch('http://quotes.stormconsultancy.co.uk/random.json');
+    const data = res.json()
+    setTimeout( () => {
+        console.log(res)
+        console.log(data)
+      }, 
+    5000);
+  };
+
+  return (
+    <div id='quote-box'>
+      <h3 id='text'>{quoteText}</h3>
+      <p id='author'>{quoteAuthor}</p>
+      <a id='tweet-quote' href='twitter.com/intent/tweet'>Twitter</a>
+      <button id='new-quote' onClick={fetchQuote}>New Quote</button>
+    </div>
+  )
+};
 
 export default Quote;
