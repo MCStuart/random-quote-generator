262fde9790af5ee41be6f310f9d2fc295dd49b93
Stuart McKay <stuart51994@gmail.com>
2020-04-03 16:04:50 -0700

    background will now fill window without borders

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import styled from 'styled-components';
 const Grid = styled.div`
   display: grid;
   grid-template: repeat(3, 1fr) / repeat(3, 1fr);
-  height: 100%;
-  width: 100%;
+  height: 100vh;
+  width: 100vw;
   background-color: lightblue;
   background-size: stretch;
 
